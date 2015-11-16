68c604179ed88b177d063a401d050f55273a21ac
Nguyen Trung Duc <ngtrduc1994@gmail.com>
2015-11-16 22:42:31 +0700

    active and password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,10 @@ before_action :admin_user,     only: :destroy
     def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      UserMailer.account_activation(@user).deliver_now
+	@user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
